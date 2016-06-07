5daf6650558652b54c8f8c1a7c42063f458e095b
tyeung <tyeung@walmartlabs.com>
2016-06-06 23:17:55 -0700

    Got the left swipe to show/hide icons and change colors correctly

--- Mailbox/MailboxViewController.swift
+++ Mailbox/MailboxViewController.swift
@@ -17,6 +17,8 @@ class MailboxViewController: UIViewController {
     @IBOutlet weak var singleMsgContainerView: UIView!
     var singleMsgContainerOriginalCenter: CGPoint!
 
+    @IBOutlet weak var laterIconImageView: UIImageView!
+    @IBOutlet weak var listIconImageView: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -33,39 +35,91 @@ class MailboxViewController: UIViewController {
 
     }
     
+    func isGoingLeft (velocity: CGPoint) -> Bool {
+        if (velocity.x > 0) {
+            return false
+        } else {
+            return true
+        }
+    }
+    
     @IBAction func didPanSingleMsgContainer(sender: UIPanGestureRecognizer) {
       let translation = sender.translationInView(view)
       
       //Offset constants for left swipe
       let minLeftTransitionDistance = CGFloat(60.0)
+      let minLeftListingDistance = CGFloat(260.0)
       let minDistanceLeftSwipe = CGFloat(40.0)
-      let laterIconOffset = CGFloat(30.0)
-      let laterIconPos = singleMsgContainerView.frame.origin.x + singleMsgContainerView.frame.size.width + laterIconOffset
+      let laterIconOffset = CGFloat(10.0)
+      let leftSwipeIconPos = singleMsgView.frame.origin.x + singleMsgView.frame.size.width + laterIconOffset
         
       //use velocity to deterimine if it's going left or right
       let velocity = sender.velocityInView(view)
-        
-
         
         if sender.state == UIGestureRecognizerState.Began {
             //Save the single message view container's original center
             singleMsgContainerOriginalCenter = singleMsgContainerView.center
             
-            if abs(singleMsgContainerView.frame.origin.x) < minDistanceLeftSwipe {
+            print("panning BEGAN")
+        } else if sender.state == UIGestureRecognizerState.Changed {
+            print("panning CHANGED")
+            print("panning x \(abs(singleMsgContainerView.frame.origin.x))")
+            
+            //current x distance made to the left
+            let xDistance = abs(singleMsgContainerView.frame.origin.x)
+            
+            //animate the later icon
+            UIView.animateWithDuration(0.3, animations: { () -> Void in
+                self.laterIconImageView.alpha = 1
+            })
+
+            if xDistance > 0 &&  xDistance < minDistanceLeftSwipe {
                 //Update the background color to gray when the panning starts
                 print("updating background color to gray")
                 self.view.backgroundColor = UIColor.lightGrayColor()
             }
-            print("panning BEGAN")
-        } else if sender.state == UIGestureRecognizerState.Changed {
-            print("panning CHANGED")
+            
+            if (xDistance > minDistanceLeftSwipe && xDistance < minLeftTransitionDistance) || (xDistance < minLeftListingDistance && xDistance > minLeftTransitionDistance) {
+                //initialize the later icon position
+                laterIconImageView.frame.origin.x = leftSwipeIconPos
+                
+                //hide the listing icon
+                listIconImageView.alpha = 0
+                
+                //update the color to yellow
+                print("updating background color to yellow")
+                self.view.backgroundColor = UIColor(red:255/255, green: 204/255, blue: 0/255, alpha: 1.0)
+
+                //update later icon frame position
+                self.laterIconImageView.frame = laterIconImageView.frame
+            }
+            
+            if xDistance > minLeftListingDistance {
+                //hide the later icon
+                laterIconImageView.alpha = 0
+            
+                //show the listing icon
+                listIconImageView.alpha = 1
+                
+                //initialize the later icon position
+                listIconImageView.frame.origin.x = leftSwipeIconPos
+                
+                //update the color to brown
+                self.view.backgroundColor = UIColor(red:216/255, green: 165/255, blue: 117/255, alpha: 1.0)
+                
+                //update listing icon frame position
+                self.listIconImageView.frame = listIconImageView.frame
+            }
+
+            //keep moving the single message view container
             singleMsgContainerView.center = CGPoint(x: singleMsgContainerOriginalCenter.x + translation.x, y: singleMsgContainerOriginalCenter.y)
+
         } else if sender.state == UIGestureRecognizerState.Ended {
             print("panning ENDED")
             print("panning x \(abs(singleMsgContainerView.frame.origin.x))")
             
             if abs(singleMsgContainerView.frame.origin.x) < minLeftTransitionDistance {
-                
+                laterIconImageView.alpha = 0
                 // Animate the message back into its original position
                 UIView.animateWithDuration(0.2, animations: { () -> Void in
                     self.singleMsgContainerView.frame.origin.x = self.singleMsgContainerView.frame.size.width - 320
